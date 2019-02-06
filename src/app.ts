89cdc721aa2c08ea53b782fdfa6399de7861cf8e
rohi <rohi040404@gmail.com>
2019-02-06 22:41:32 +0900

    Change port number

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import * as Messaging from "./utils/messaging";
 import { AddressInfo } from "net";
 import { createAppToRun } from "./create-app";
 
-const server = createAppToRun().listen(3456, function () {
+const server = createAppToRun().listen(80, function () {
     const port = server.address() as AddressInfo;
     console.log("Express server listening on port " + port.port);
     loadQueue().then(() => {
